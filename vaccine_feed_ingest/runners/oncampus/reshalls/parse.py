dedf425e668601eb69dc51b864afa36daa99da61
Adrian Edwards <adrian@adriancedwards.com>
2023-06-11 09:38:26 -0700

    convert the reshalls parser into one compatible with the existing pipeline

--- vaccine_feed_ingest/runners/oncampus/reshalls/parse.py
+++ vaccine_feed_ingest/runners/oncampus/reshalls/parse.py
@@ -1,16 +1,37 @@
-import requests
+#!/usr/bin/env python
+
+#
+# Parse stage should convert raw data into json records and store as ndjson.
+#
+
+import json
+import pathlib
+import re
+import sys
+from typing import Dict, List
+
 from bs4 import BeautifulSoup
 
-URL = "https://www.rit.edu/housing/residence-halls"
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
 
-results = soup.find_all("div", class_="view-rates")
-for result in results:
-    roomSizes = result.find_all("div",class_="field--name-field-room-size")
-    singleSemCosts = result.find_all("div",class_="field--name-field-per-semester-per-person")
-    twoSemCosts = result.find_all("div",class_="field--name-field-per-2-semesters-per-person")
+def soupify_file(input_path: pathlib.Path) -> List[BeautifulSoup]:
+    """Opens up a provided file path, feeds it into BeautifulSoup.
+    Returns a new BeautifulSoup object for each found table
+    """
+    with open(input_path, "r") as fd:
+        return BeautifulSoup(fd, "html.parser")
+
+
+def extract_room_info(page: BeautifulSoup) -> Dict[str, str]:
+    """Takes a BeautifulSoup tag corresponding to a page as input,
+    Returns a dict of labeled data, suitable for transformation into ndjson
+    """
+    result: Dict[str, str] = {}
+
+
+    roomSizes = page.find_all("div",class_="field--name-field-room-size")
+    singleSemCosts = page.find_all("div",class_="field--name-field-per-semester-per-person")
+    twoSemCosts = page.find_all("div",class_="field--name-field-per-2-semesters-per-person")
     roomSizeArr = []
     singSemCostArr = []
     twoSemCostArr = []
@@ -21,7 +42,39 @@ for result in results:
     for twoSemCost in twoSemCosts:
         twoSemCostArr.append(twoSemCost.text)
 
+    result["rooms"] = []
+
     for i in range(len(roomSizeArr)):
-        print(roomSizeArr[i])
-        print(singSemCostArr[i])
-        print(twoSemCostArr[i])
+        roomStyle = {}
+        roomStyle["size"](roomSizeArr[i])
+        roomStyle["cost"] = {
+            "semster": singSemCostArr[i],
+            "year": twoSemCostArr[i],
+            "summer":""
+        }
+        result["rooms"].append(roomStyle)
+
+    return result
+
+if __name__ == "__main__":
+    output_dir = pathlib.Path(sys.argv[1])
+    input_dir = pathlib.Path(sys.argv[2])
+
+    for html in input_dir.glob("**/*.html"):
+        if not html.is_file():
+            continue
+        data = []
+        
+        for result in soupify_file(html).find_all("div", class_="view-rates"):
+
+            data.extend(
+                [
+                    extract_room_info(result)
+                ]
+            )
+
+
+        output = "\n".join(json.dumps(item) for item in data)
+        outpath = output_dir / (html.with_suffix(".parsed.ndjson").name)
+        with open(outpath, "w") as fd:
+            fd.write(output)
