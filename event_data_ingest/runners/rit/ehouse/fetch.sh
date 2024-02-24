b27a0f4488ae1e02046bc4ca8763d7b8793b53b8
Adrian Edwards <adrian@adriancedwards.com>
2024-02-24 13:16:23 -0500

    initial ehouse scraper

---
+++ event_data_ingest/runners/rit/ehouse/fetch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#
+# Fetch stage should retrieve raw data from external source and store it unmodified
+#
+
+set -Eeuo pipefail
+
+output_dir=""
+
+if [ -n "${1}" ]; then
+    output_dir="${1}"
+else
+    echo "Must pass an output_dir as first argument"
+fi
+
+### Replace the following with your implementation ###
+
+echo "Fetching into ${output_dir}"
+touch "${output_dir}/data.geojson"
+
+###
+
+(cd "$output_dir" && curl --silent "https://calendar.google.com/calendar/ical/enghouseevents%40gmail.com/public/basic.ics" -o 'ehouse.ics')
