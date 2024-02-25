badfaabf9310fe0b4a6534640ed0fa7551a2b0f9
Adrian Edwards <adrian@adriancedwards.com>
2024-02-25 07:35:20 -0500

    RIT events fetcher

---
+++ event_data_ingest/runners/rit/events/fetch.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+import os
+import pathlib
+import sys
+import urllib.parse
+
+import requests
+import yaml
+from bs4 import BeautifulSoup
+
+from event_data_ingest.utils.log import getLogger
+
+logger = getLogger(__file__)
+
+output_dir = sys.argv[1]
+
+
+base_url = urllib.parse.urlparse("https://www.rit.edu/events/")
+headers = {}
+extra_params = {}
+page = 0
+while True:
+
+	print("loop")
+	params = urllib.parse.urlencode({"page": page})
+	url = f"{base_url.geturl()}?{params}"
+	response = requests.get(url, allow_redirects=False, headers=headers)
+	content = response.text
+
+	with open(os.path.join(output_dir, f"{page}.html"), "w") as f:
+		f.write(content)
+
+	soup = BeautifulSoup(content, "html.parser")
+	current_page = int(list(soup.find('a', title="Current page").children)[-1])
+	if current_page != (page + 1):
+		break
+	page += 1
+		
