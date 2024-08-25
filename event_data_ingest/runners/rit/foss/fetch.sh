354ad44a47e6b533e506c64ce8afba6a885e9d8a
Adrian Edwards <adrian@adriancedwards.com>
2024-08-24 22:41:42 -0400

    add FOSSRIT calendar

---
+++ event_data_ingest/runners/rit/foss/fetch.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#
+# Fetch stage should retrieve raw data from external source and store it unmodified
+#
+
+set -Eeuo pipefail
+
+output_dir=""
+
+if [ -n "${1}" ]; then
+    output_dir="${1}"
+else
+    echo "Must pass an output_dir as first argument"
+fi
+
+### Replace the following with your implementation ###
+
+echo "Fetching into ${output_dir}"
+touch "${output_dir}/events.ics"
+
+###
+
+(cd "$output_dir" && curl --silent "https://fossrit.github.io/feeds/calendar.ics" -o 'events.ics')
